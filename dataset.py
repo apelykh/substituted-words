e94c06725a9f1dd378ef33cdabd687f57b714667
Anton Pelykh <anton.pelykh@gmail.com>
2021-01-08 22:57:59 +0000

    LSTM -> BERT

--- dataset.py
+++ dataset.py
@@ -3,6 +3,7 @@ import torch
 import numpy as np
 import collections
 from torch.utils import data
+from transformers import BertTokenizer
 
 
 class TextDataset(data.Dataset):
@@ -16,13 +17,16 @@ class TextDataset(data.Dataset):
         self.max_len = max_len
         print('[DATASET] Maximum line length: {}'.format(max_len))
 
-        self.vocab = vocab if vocab else self._create_vocabulary(freq_threshold=freq_threshold)
-        print('[DATASET] Vocabulary size: {}'.format(len(self.vocab)))
+        # self.vocab = vocab if vocab else self._create_vocabulary(freq_threshold=freq_threshold)
+        # print('[DATASET] Vocabulary size: {}'.format(len(self.vocab)))
+        #
+        # self.word2id = {word: i + 2 for i, word in enumerate(self.vocab)}
+        # self.unk_index = 1
+        # self.word2id['<UNK>'] = self.unk_index
+        # self.word2id['<PAD>'] = 0
 
-        self.word2id = {word: i + 2 for i, word in enumerate(self.vocab)}
-        self.unk_index = 1
-        self.word2id['<UNK>'] = self.unk_index
-        self.word2id['<PAD>'] = 0
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased',
+                                                       do_lower_case=True)
 
     def _read_gt_files(self, base_path, split_name):
         src_text_path = os.path.join(base_path, '{}.src'.format(split_name))
@@ -44,34 +48,57 @@ class TextDataset(data.Dataset):
 
         return vocab
 
+    def _tokenize_and_preserve_labels(self, sentence, text_labels):
+        tokenized_sentence = []
+        labels = []
+
+        for word, label in zip(sentence, text_labels):
+            # Tokenize the word and count # of subwords the word is broken into
+            tokenized_word = self.tokenizer.tokenize(word)
+            tokenized_sentence.extend(tokenized_word)
+
+            # Add the same label to the new list of labels `n_subwords` times
+            n_subwords = len(tokenized_word)
+            labels.extend([label] * n_subwords)
+
+        return tokenized_sentence, labels
+
     def __getitem__(self, index: int):
         line = self.preprocess_line(self.lines[index])
-        line_len = min(len(line), self.max_len)
+        labels = self.preprocess_line(self.labels[index])
+
+        # some words are tokenized to subword units -> labels are extended to each subword part
+        tokenized_line, labels = self._tokenize_and_preserve_labels(line, labels)
+
+        line_len = min(len(tokenized_line), self.max_len)
 
         encoded_line = np.zeros(self.max_len)
-        encoded_line[:line_len] = np.array([self.word2id.get(word, self.unk_index)
-                                            for word in line[:line_len]])
+        encoded_line[:line_len] = np.array([self.tokenizer.convert_tokens_to_ids(token)
+                                            for token in tokenized_line[:line_len]])
 
         line_labels = np.zeros(self.max_len)
-        line_labels[:line_len] = self.labels[index].split(' ')[:line_len]
+        line_labels[:line_len] = labels[:line_len]
+
+        mask = np.zeros(self.max_len)
+        mask[:line_len] = 1.0
 
         return torch.as_tensor(encoded_line, dtype=torch.long),\
             torch.as_tensor(line_labels, dtype=torch.long),\
-            line_len
+            torch.as_tensor(mask)
 
     def __len__(self) -> int:
         return len(self.lines)
 
-    def get_vocab(self):
-        return self.vocab
+    # def get_vocab(self):
+    #     return self.vocab
 
 
 if __name__ == '__main__':
     d = train_dataset = TextDataset(base_path='./data',
                             split_name='train_small',
-                            max_len=None)
+                            max_len=100)
 
-    encoded_line, line_labels, line_len = d[0]
-    print(encoded_line.size())
-    print(line_labels.size())
-    print(line_len)
+    encoded_line, line_labels, mask = d[58]
+    print(encoded_line)
+    print(line_labels)
+    print(mask)
--- model_trainer.py
+++ model_trainer.py
@@ -1,5 +1,9 @@
 import os
 import torch
+import numpy as np
+# from seqeval.metrics import f1_score, accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from transformers import get_linear_schedule_with_warmup
 
 
 class ModelTrainer:
@@ -11,71 +15,129 @@ class ModelTrainer:
         self.model_name = model_name
         self.model = model
         self.optimizer = optimizer
+        self.lr_scheduler = None
         self.criterion = criterion
         self.device = device
+        self.max_grad_norm = 1.0
 
     def _validate_epoch(self, val_loader):
-        running_val_loss = 0.0
+        total_val_loss = 0.0
+        predicted_labels = []
+        true_labels = []
 
-        with torch.no_grad():
-            for i, batch in enumerate(val_loader):
-                lines, labels, line_lengths = batch
-                # labels = labels.contiguous().view(-1)
+        for i, batch in enumerate(val_loader):
+            batch = tuple(elem.to(self.device) for elem in batch)
+            lines, labels, masks = batch
 
-                # predictions = self.model(lines.to(self.device))
-                predictions = self.model(lines.to(self.device), line_lengths.int())
-                # b = predictions.size(1)
-                # t = predictions.size(0)
-                # predictions = predictions.view(b * t, -1)
+            with torch.no_grad():
+                outputs = self.model(lines,
+                                    token_type_ids=None,
+                                    attention_mask=masks,
+                                    labels=labels)
 
-                loss = self.criterion(torch.squeeze(predictions).float(), labels.to(self.device).float())
+            total_val_loss += outputs[0].item()
 
-                # loss = self.model.loss(predictions, labels.to(self.device), line_lengths.to(self.device))
-                running_val_loss += loss.item()
+            # (32, 200, 2)
+            logits = outputs[1].detach().cpu().numpy()
+            predictions = np.argmax(logits, axis=2)
+            batch_size, seq_len = predictions.shape
 
-        return running_val_loss / len(val_loader)
+            predicted_labels.extend(list(predictions.reshape(batch_size * seq_len).astype(np.int)))
+            t_labels = labels.to('cpu').numpy()
+            true_labels.extend(list(t_labels.reshape(batch_size * seq_len).astype(np.int)))
+
+        print("Validation Accuracy: {}".format(accuracy_score(true_labels, predicted_labels)))
+        print("Precision: {}".format(precision_score(true_labels, predicted_labels)))
+        print("Recall: {}".format(recall_score(true_labels, predicted_labels)))
+        print("Validation F1-Score: {}".format(f1_score(true_labels, predicted_labels)))
+
+        return total_val_loss / len(val_loader)
+
+    # -------------------------------------------------------------------------------------------
+    #     # Put the model into evaluation mode
+    #     model.eval()
+    #     # Reset the validation loss for this epoch.
+    #     eval_loss, eval_accuracy = 0, 0
+    #     predictions, gt = [], []
+    #
+    #     for batch in valid_dataloader:
+    #         batch = tuple(t.to(device) for t in batch)
+    #         b_input_ids, b_input_mask, b_labels = batch
+    #
+    #         with torch.no_grad():
+    #             outputs = model(b_input_ids, token_type_ids=None,
+    #                             attention_mask=b_input_mask, labels=b_labels)
+    #         # Move logits and labels to CPU
+    #         logits = outputs[1].detach().cpu().numpy()
+    #         label_ids = b_labels.to('cpu').numpy()
+    #
+    #         # Calculate the accuracy for this batch of test sentences.
+    #         eval_loss += outputs[0].mean().item()
+    #         predictions.extend([list(p) for p in np.argmax(logits, axis=2)])
+    #         gt.extend(label_ids)
+    #
+    #     eval_loss = eval_loss / len(valid_dataloader)
+    #     validation_loss_values.append(eval_loss)
+    #     print("Validation loss: {}".format(eval_loss))
+    #
+    #     pred_tags = [tag_values[p_i] for p, l in zip(predictions, gt)
+    #                  for p_i, l_i in zip(p, l) if tag_values[l_i] != "PAD"]
+    #     valid_tags = [tag_values[l_i] for l in gt
+    #                   for l_i in l if tag_values[l_i] != "PAD"]
+    #     print("Validation Accuracy: {}".format(accuracy_score(pred_tags, valid_tags)))
+    #     print("Validation F1-Score: {}".format(f1_score(pred_tags, valid_tags)))
+    # -------------------------------------------------------------------------------------------
 
     def _train_epoch(self, train_loader):
-        running_loss = 0.0
+        total_loss = 0.0
 
         for i, batch in enumerate(train_loader):
-            # print(batch['train_features'].shape)
-            self.optimizer.zero_grad()
+            lines, labels, masks = batch
 
-            lines, labels, line_lengths = batch
-            # labels = labels.contiguous().view(-1)
+            self.model.zero_grad()
 
-            # predictions = self.model(lines.to(self.device))
-            predictions = self.model(lines.to(self.device), line_lengths.int())
-            # b = predictions.size(1)
-            # t = predictions.size(0)
-            # predictions = predictions.view(b * t, -1)
+            outputs = self.model(lines.to(self.device),
+                                token_type_ids=None,
+                                attention_mask=masks.to(self.device),
+                                labels=labels.to(self.device))
 
-            loss = self.criterion(torch.squeeze(predictions).float(), labels.to(self.device).float())
-
-            # loss = self.model.loss(predictions, labels.to(self.device), line_lengths.to(self.device))
+            loss = outputs[0]
+            total_loss += loss.item()
             loss.backward()
-            self.optimizer.step()
 
             every_n_batches = 30
             if i % every_n_batches == every_n_batches - 1:
                 print('[%d/%4d] loss: %.3f' % (i + 1, len(train_loader), loss.item()))
 
-            running_loss += loss.item()
-        return running_loss / len(train_loader)
+            # Clip the norm of the gradient to help prevent the exploding gradients problem.
+            torch.nn.utils.clip_grad_norm_(parameters=self.model.parameters(),
+                                           max_norm=self.max_grad_norm)
+            self.optimizer.step()
+            self.lr_scheduler.step()
+
+        return total_loss / len(train_loader)
 
     def fit(self, train_loader, val_loader, start_epoch, num_epochs):
         train_loss = []
         val_loss = []
 
+        self.lr_scheduler = get_linear_schedule_with_warmup(
+            self.optimizer,
+            num_warmup_steps=0,
+            num_training_steps=len(train_loader) * num_epochs
+        )
+
         for epoch in range(start_epoch, start_epoch + num_epochs):
-            print('Epoch {}/{}'.format(epoch, start_epoch + num_epochs - 1))
-            avg_epoch_loss = self._train_epoch(train_loader)
-            train_loss.append(avg_epoch_loss)
-            print('Epoch {} train loss: {:.4f}'.format(epoch, avg_epoch_loss))
+            # self.model.train()
+            # print('Epoch {}/{}'.format(epoch, start_epoch + num_epochs - 1))
+            # avg_epoch_loss = self._train_epoch(train_loader)
+            avg_epoch_loss = 0
+            # train_loss.append(avg_epoch_loss)
+            # print('Epoch {} train loss: {:.4f}'.format(epoch, avg_epoch_loss))
 
             # ---------------------------------------------------------------
             # validation
+            self.model.eval()
             avg_epoch_val_loss = self._validate_epoch(val_loader)
             val_loss.append(avg_epoch_val_loss)
             print('Epoch {} val loss: {:.4f}'.format(epoch, avg_epoch_val_loss))
